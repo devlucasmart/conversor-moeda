013b413090ff9f6e93f64593be04b9f3f64688b6
Lucas Martins <80967565@telefonicati.onmicrosoft.com>
2025-06-20 11:24:28 -0300

    feat: Readme

--- src/main/java/org/devlucas/ConversorMoeda.java
+++ src/main/java/org/devlucas/ConversorMoeda.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.devlucas;
 
 import com.google.gson.Gson;
 import io.github.cdimascio.dotenv.Dotenv;
--- src/main/java/org/devlucas/SimbolosMoeda.java
+++ src/main/java/org/devlucas/SimbolosMoeda.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.devlucas;
 
 import java.util.Map;
 
